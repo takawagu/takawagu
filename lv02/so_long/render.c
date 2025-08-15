288dc6c4402e0e5a3be18762e790053bfc5a4f31
Takuro Kawaguchi <takawagu@c5r2s10.42tokyo.jp>
2025-08-15 18:16:03 +0900

    review修正

--- lv02/so_long/render.c
+++ lv02/so_long/render.c
@@ -6,7 +6,7 @@
 /*   By: takawagu <takawagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 18:18:12 by takawagu          #+#    #+#             */
-/*   Updated: 2025/08/05 12:47:33 by takawagu         ###   ########.fr       */
+/*   Updated: 2025/08/15 18:13:41 by takawagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,10 @@ void	draw_tile(t_game *game, int x, int y)
 			* TILE_SIZE, y * TILE_SIZE);
 		return ;
 	}
-	mlx_put_image_to_window(game->mlx, game->win, game->images.floor.img, x
-		* TILE_SIZE, y * TILE_SIZE);
-	if (game->enemy.x == x && game->enemy.y == y)
+	if (tile == '0')
+		mlx_put_image_to_window(game->mlx, game->win, game->images.floor.img, x
+			* TILE_SIZE, y * TILE_SIZE);
+	else if (game->enemy.x == x && game->enemy.y == y)
 		mlx_put_image_to_window(game->mlx, game->win, game->images.enemy.img, x
 			* TILE_SIZE, y * TILE_SIZE);
 	else if (tile == 'C')
@@ -37,6 +38,8 @@ void	draw_tile(t_game *game, int x, int y)
 	else if (tile == 'P')
 		mlx_put_image_to_window(game->mlx, game->win, game->images.player.img, x
 			* TILE_SIZE, y * TILE_SIZE);
+	else
+		exit_error(game, "Unknown tile type in map.");
 }
 
 void	draw_map(t_game *game)
